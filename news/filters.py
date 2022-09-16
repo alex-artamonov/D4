820fb6b97da97bede311b069d075a34e2d9d0fe7
alex-artamonov <alex-artamonov2010@gmail.com>
2022-09-17 01:38:54 +0300

    first commit

---
+++ news/filters.py
@@ -0,0 +1,13 @@
+from django_filters import FilterSet
+from .models import PostAuthor
+
+class NewsFilter(FilterSet):
+    
+    class Meta:
+        model = PostAuthor
+        # fields = ('created_dtm', 'title', 'author_id.)
+        fields = {
+            'Дата_создания': ['gt', 'lt'],
+            'Название_статьи': ['icontains'],
+            'Автор': ['icontains']
+        }
