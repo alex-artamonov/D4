45e7fe83ec66c9b657e52d5e89444d7384c5d77c
alex-artamonov <alex-artamonov2010@gmail.com>
2022-09-17 02:03:46 +0300

    add links to author's list

--- news/urls.py
+++ news/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     # path — означает путь. В данном случае путь ко всем товарам у нас останется пустым, позже станет ясно почему
     path('', NewsList.as_view(), name='all_news'),
     path('<int:pk>', NewsDetail.as_view(), name='news_detail'),
-    path('search', NewsF.as_view()),
+    path('search', NewsF.as_view(), name='search_news'),
     path('add/', NewsCreate.as_view(), name='add_news'),
     path('<int:pk>/delete/', NewsDelete.as_view(), name='delete_news'),
     path('<int:pk>/edit/', NewsUpdate.as_view(), name='edit_news'),
