cca65069d7d910b0ef2b13dcaec23f3ee7d61c01
alex-artamonov <alex-artamonov2010@gmail.com>
2022-09-17 14:16:50 +0300

    improved starting url

--- my_shop/urls.py
+++ my_shop/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('pages/', include('django.contrib.flatpages.urls')),
     path('products/', include('simpleapp.urls')),
+    path('', include('news.urls')),
     path('news/', include('news.urls'))
 ]
